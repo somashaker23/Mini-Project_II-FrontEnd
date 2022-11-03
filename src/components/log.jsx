6de0f4a574fce20b912f03c33ca9a5ea6f3a2acd
juniorcoder <somashaker23@gmail.com>
2022-11-03 22:07:21 +0530

    basic interface with pagination

--- src/components/log.jsx
+++ src/components/log.jsx
@@ -9,6 +9,7 @@ const log = ()=>{
                 <div><span>01/01/2022</span></div>
                 <div><span>12:00</span></div>
                 <div><span>16:00</span></div>
+
             </div>
             
             
